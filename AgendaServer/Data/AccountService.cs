5e3e559634392a6d71b51ef28010cb93a6da1f0a
soaresmessiasg130 <soaresmessiasg130@gmail.com>
2020-07-16 21:01:32 -0300

    Initial Commit

---
+++ AgendaServer/Data/AccountService.cs
@@ -0,0 +1,13 @@
+using static System.Console;
+
+namespace AgendaServer
+{
+  public class AccountService
+  {
+    public static string nome = "Messias Soares";
+
+    public AccountService () {
+      WriteLine(nome);
+    }
+  }
+}
